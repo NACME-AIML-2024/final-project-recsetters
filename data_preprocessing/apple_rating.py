6ce739b74f0ebeb412ff94e32f81ec27d71fd81b
ayalaa04 <113703122+ayalaa04@users.noreply.github.com>
2024-07-25 14:15:23 -0700

    ran apple py files

--- data_preprocessing/apple_rating.py
+++ data_preprocessing/apple_rating.py
@@ -1,26 +1,31 @@
 import pandas as pd
+import ast
+
 
 # Load CSV data
-df_user = pd.read_csv('user.csv')
-df_apple_music = pd.read_csv('items.csv')
+df_user = pd.read_csv('../data_collection/DATASET/user.csv')
+df_apple_music = pd.read_csv('../data_collection/DATASET/apple_music_dataset.csv')
 
 # Assign IDs to DataFrames
 df_apple_music['id'] = range(1, len(df_apple_music) + 1)
 df_user['id'] = range(1, len(df_user) + 1)
 
-# Create a dictionary mapping track IDs to track names
-id_to_trackname = pd.Series(df_apple_music['trackName'].values, index=df_apple_music['id']).to_dict()
+# Create a dictionary mapping track names to IDs
+trackname_to_id = pd.Series(df_apple_music['id'].values, index=df_apple_music['trackName']).to_dict()
 
 id_lst = []
 index_count = {}
 df_lst = []
+skipped_songs = []
+missing_users = []
 
 for i in range(len(df_user)):
     loved_tracks_str = df_user.loc[i, 'loved_tracks']
+    username = df_user.loc[i, 'name']
     
     start_marker = "'track_name': '"
     end_marker = "',"
-    start_marker_2 = "'uts': '"
+    start_marker_2 = "'#text': '"
     end_marker_2 = "'"
     
     track_names = []
@@ -54,43 +59,67 @@ for i in range(len(df_user)):
         start_pos = end_pos + len(end_marker)
 
     for name, date in zip(track_names, date_times):
-        print(i)
         if name in df_apple_music['trackName'].values:
             index = df_apple_music.index[df_apple_music['trackName'] == name].tolist()[0]
-            id_lst = [i, index + 1]  # Add 1 to match track_id
+            id_lst = [username, name, date]
             if index in index_count:
                 index_count[index] += 1
             else:
                 index_count[index] = 1
-            df_lst.append(id_lst + [date])
+            df_lst.append(id_lst)
+        else:
+            skipped_songs.append(name)
 
 # Create DataFrame from the extracted data
-df_result = pd.DataFrame(df_lst, columns=['user_id', 'track_id', 'date_time'])
+df_result = pd.DataFrame(df_lst, columns=['username', 'track_name', 'date_time'])
+
+# Remove rows where track_name is "Unknown"
+df_result = df_result[df_result['track_name'] != 'Unknown']
+
+df_result = df_result.drop_duplicates(subset=['username','track_name'], keep='last').reset_index(drop=True)
+
 # Filter interactions based on the given criteria
-df_duplicates = df_result[df_result.duplicated(subset=['user_id', 'track_id'], keep=False)]
-
-df_result = df_result[~df_result.set_index(['user_id', 'track_id']).index.isin(df_duplicates.set_index(['user_id', 'track_id']).index)]
-
 while True:
-    song_lst = [key for key, value in index_count.items() if value < 2]
-    df_result = df_result[~df_result['track_id'].isin(song_lst)]
+    song_lst = [track for track, count in index_count.items() if count < 5]
+    df_result = df_result[~df_result['track_name'].isin(song_lst)]
     
-    user_interaction_count = df_result['user_id'].value_counts()
-    low_interaction_users = user_interaction_count[user_interaction_count < 2].index.tolist()
+    user_interaction_count = df_result['username'].value_counts()
+    low_interaction_users = user_interaction_count[user_interaction_count < 5].index.tolist()
     
     if not low_interaction_users and not song_lst:
         break
     
-    df_result = df_result[~df_result['user_id'].isin(low_interaction_users)]
+    df_result = df_result[~df_result['username'].isin(low_interaction_users)]
     
-    updated_track_count = df_result['track_id'].value_counts()
+    updated_track_count = df_result['track_name'].value_counts()
     index_count = updated_track_count.to_dict()
 
+# Check if every username in df_result is in df_user
+for username in df_result['username'].unique():
+    if username not in df_user['name'].values:
+        missing_users.append(username)
+
+# Log the missing users
+if missing_users:
+    print("Missing Users:")
+    for user in missing_users:
+        print(user)
+
+# Convert 'date_time' to Unix time
+df_result['date_time'] = pd.to_datetime(df_result['date_time'], format='%d %b %Y, %H:%M').astype('int64') // 10**9
+
+# Sort the DataFrame by 'username' and 'date_time'
+df_result.sort_values(by=['username', 'date_time'], ascending=[True, True], inplace=True)
+
 # Reset index if needed
-
-# Reindex user_id to ensure they are continuous without any gaps
-user_id_mapping = {old_id: new_id for new_id, old_id in enumerate(sorted(df_result['user_id'].unique()))}
-df_result['user_id'] = df_result['user_id'].map(user_id_mapping)
-
 df_result.reset_index(drop=True, inplace=True)
-df_result.to_csv('final_interactions.csv', index=False)
+df_result.to_csv('../data_collection/DATASET/ratings.csv', index=False)
+
+# Print skipped songs
+print("Skipped Songs:", len(skipped_songs))
+
+df_result.sort_values(by=['username', 'date_time'], ascending=[True, True], inplace=True)
+
+# Reset index if needed
+df_result.reset_index(drop=True, inplace=True)
+print('Df result:\n', df_result)
